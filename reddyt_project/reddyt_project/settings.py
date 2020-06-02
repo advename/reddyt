6b293a975da517e285a7908bd6fccf3468233ddd
Lars <you@example.com>
2020-06-02 20:15:40 +0200

    Without task queues

--- reddyt_project/reddyt_project/settings.py
+++ reddyt_project/reddyt_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'YOUR_EMAIL_ACCOUNT@gmail.com'
-EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
+EMAIL_HOST_USER = config.email['account']
+EMAIL_HOST_PASSWORD = config.email['password']
 
 
 # Internationalization
